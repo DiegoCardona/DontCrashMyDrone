6ae98be86fff7633dfc7a00bd91408210c47489a
DiegoCardona <cardona.root@gmail.com>
2016-04-23 14:32:58 -0500

    socket configuration base line

--- ServerNinja/server/app.js
+++ ServerNinja/server/app.js
@@ -9,3 +9,16 @@ mongoose.connect('mongodb://localhost:27017/chat');
 var NFZ = require('./models/NFZModel');
 var NFZ = require('./models/WAZModel');
 var NFZ = require('./models/DroneModel');
+
+// Socket logic
+socket.on('connection', function(sk) {
+
+	clients[sk.client.id] = {
+		socket: socket,
+		username: '',
+		userType: ''
+	};
+	sk.on('userConnected', function(data) {
+		sk.emit('confirmConnection');
+	});
+});
