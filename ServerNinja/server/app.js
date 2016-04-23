72705aeb05bf5e778cad18caef42bbfcc8f58844
DiegoCardona <cardona.root@gmail.com>
2016-04-23 14:20:13 -0500

    moves the call to models to an appropiated place

--- ServerNinja/server/app.js
+++ ServerNinja/server/app.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 var socket = require('socket.io')();
 var mongoose = require('mongoose');
-var models = require('./models');
 
 mongoose.connect('mongodb://localhost:27017/chat');
+var models = require('./models');
