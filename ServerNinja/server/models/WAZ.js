8fac36eb41f678e3d7b20f0cb0784c3f80eb8b3b
DiegoCardona <cardona.root@gmail.com>
2016-04-23 14:08:29 -0500

    add weather alert zones persistence

---
+++ ServerNinja/server/models/WAZ.js
@@ -0,0 +1,27 @@
+/*model thtat define de Weather alert Zones*/
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+	latitude: {
+		type: Number,
+		default: 0.0
+	}
+	longitude: {
+		type: Number,
+		default: 0.0
+	},
+	radio: {
+		type: Number,
+		default: 1
+	},
+	description: {
+		type: String,
+		default: 'No description Assgined'
+	},
+	warinig_levet: {
+		type: String,
+		default: 'low'
+	}
+});
+
+module.exports = mongoose.model('WAZ', schema);
